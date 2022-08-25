1c2ff640ef723cff491df3caed1a4a70b41172a1
Kcrespo15 <kevincrespo15@gmail.com>
2022-08-25 03:21:15 -0400

    finished main java

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,55 @@ function displayTime() {
    $("#currentDay").text(currentTime);
 }
 
-setInterval(displayTime);
+
 
 // function for time block
+
+function timeBlocks () {
+   var currentHour = moment().hour();
+
+   $(".time-block").each(function(){
+      var hour = parseInt($(this).attr('id'));
+      // console.log(hour)
+      // console.log(currentHour)
+
+      if ( currentHour < hour ) {
+         $(this).addClass("future");
+        
+      } else if (currentHour === hour){
+        
+         $(this).addClass("present");
+
+      } else {
+         $(this).addClass('past')
+      }
+      
+      })
+
+}
+
+// function to save local storage 
+$('.saveBtn').on('click' , function () {
+   var cTime = $(this).parent().attr("id");
+   var textArea = $(this).siblings('.col-10').val();
+
+
+   // console.log(textArea);
+
+  
+   
+
+   localStorage.setItem(cTime , textArea);
+
+   console.log('Hello World')
+})
+
+
+$("#txtN9").val(localStorage.getItem('random'));
+$("#txtN10").val(localStorage.getItem('random'));
+
+// call functions
+
+setInterval(displayTime);
+
+timeBlocks();
